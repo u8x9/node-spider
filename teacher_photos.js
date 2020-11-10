6989a6c3cd8f650aaccc5fb42b616f3e28354b31
u8x9 <u8x@eclipso.eu>
2020-11-11 01:48:48 +0800

    使用cheerio解析html

--- teacher_photos.js
+++ teacher_photos.js
@@ -1,8 +1,10 @@
 // 引入http模块
 const http = require('http')
+const cheerio = require('cheerio')
+const HOST = 'http://web.itheima.com'
 
 // 创建请求对象(此时未发送请求)
-let req = http.request('http://web.itheima.com/teacher.html', res => {
+let req = http.request(HOST + '/teacher.html', res => {
     // 异步响应
 
     let chunks = []
@@ -15,7 +17,22 @@ let req = http.request('http://web.itheima.com/teacher.html', res => {
     // 监听end事件，数据获取完毕时触发
     res.on('end', () => {
         // 拼接所有的chunk，并转换成字符串
-        console.log(Buffer.concat(chunks).toString())
+        let html = Buffer.concat(chunks).toString()
+        let $ = cheerio.load(html)
+        // 方法1：使用each
+        //let pics = []
+        //$('.maincon .main_pic > img').each((index, item) => {
+        //pics.push(HOST + $(item).attr('src'))
+        //})
+
+        // 方法2：使用js5的Array map方法
+        let pics = Array.prototype.map.call($('.maincon .main_pic > img'), item => HOST + $(item).attr('src'))
+
+        // 方法3：使用 cheerio 的map
+        //let pics = $('.maincon .main_pic > img').map((idx, item) => {
+        //HOST + $(item).attr('src')
+        //})
+        console.log(pics)
     })
 })
 
